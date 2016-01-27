29b2872a5535df19e43af1dfad80f80905c04952
Danil <difeid@yandex.ru>
2016-01-27 14:18:42 +0300

    state tonumber

--- eventsms.lua
+++ eventsms.lua
@@ -3,7 +3,7 @@
 -- Eventhandler for SMS Tools 3
 -- Add eventhandler=/path/to/eventsms.lua into global part of smsd.conf
 -- Written by DIfeID (difeid@yandex.ru), 2016, Copyleft GPLv3 license
--- Version 1.2
+-- Version 1.3
 
 local status = arg[1]
 local path = arg[2]
@@ -128,17 +128,18 @@ do
                     table.insert(out, GPIO_NAME[i]..' off')
                 elseif string.match(cmd, GPIO_NAME[i]..' pulse') then
                     local state = capture('cat /sys/class/gpio/gpio'..GPIO_NUMBER[i]..'/value')
-                    if state == '0' then
-                        state = '1'
+                    state = tonumber(state)
+                    if state == 0 then
+                        state = 1
                     else
-                        state = '0'
+                        state = 0
                     end
                     os.execute('echo '..state..' > /sys/class/gpio/gpio'..GPIO_NUMBER[i]..'/value')
-                    sleep('5s')
-                    if state == '0' then
-                        state = '1'
+                    sleep(5)
+                    if state == 0 then
+                        state = 1
                     else
-                        state = '0'
+                        state = 0
                     end
                     os.execute('echo '..state..' > /sys/class/gpio/gpio'..GPIO_NUMBER[i]..'/value')
                     table.insert(out, GPIO_NAME[i]..' pulse')
